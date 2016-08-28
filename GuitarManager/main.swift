21e0b6ad6193644ed7e950b0301c9dc496f8a195
junginsung <hunter10@naver.com>
2016-08-28 17:49:09 +0900

    Initial Commit

---
+++ GuitarManager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GuitarManager
+//
+//  Created by junginsung on 2016. 8. 28..
+//  Copyright © 2016년 junginsung. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
