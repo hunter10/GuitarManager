8c858002da4ee2e70b2ad2cdec1d22761c4d973c
junginsung <hunter10@naver.com>
2016-10-09 20:06:09 +0900

    HeadFirst OOD Chapter5 finish

---
+++ GuitarManager/MandolinSpec.swift
@@ -0,0 +1,9 @@
+//
+//  MandolinSpec.swift
+//  GuitarManager
+//
+//  Created by junginsung on 2016. 10. 8..
+//  Copyright © 2016년 junginsung. All rights reserved.
+//
+
+import Foundation
---
+++ GuitarManager/Style.swift
@@ -0,0 +1,9 @@
+//
+//  Style.swift
+//  GuitarManager
+//
+//  Created by junginsung on 2016. 10. 8..
+//  Copyright © 2016년 junginsung. All rights reserved.
+//
+
+import Foundation
